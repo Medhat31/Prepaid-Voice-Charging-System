29a221b848eb29cd9f34515e02c4c4ab43a75618
Medhat31 <medhatkhalil340@gmail.com>
2026-06-10 21:31:03 +0300

    feat: Created the main structure of MSC

---
+++ MSC/src/main/java/com/telecom/app/Main.java
@@ -0,0 +1,14 @@
+package com.telecom.app;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mfathy
+ */
+public class Main {
+    
+}
